17d80c26bf66bd64b29df13bcaebcc62a8634274
Jason Swartz <dev@bks2.com>
2015-01-27 18:48:34 -0800

    Disabling the printlns

--- src/main/scala/jtop/Main.scala
+++ src/main/scala/jtop/Main.scala
@@ -8,10 +8,10 @@ object Main extends scala.scalajs.js.JSApp {
   import js.Dynamic.global
 
   def main(): Unit = {
-    println("Creating client")
+    // println("Creating client")
     val client = JMX.createClient(ClientConfiguration(host = "localhost", port = 8855))
 
-    println("Connecting...")
+    // println("Connecting...")
     client.connect()
     client.on("connect", () => {
       refresh(client)
@@ -24,49 +24,49 @@ object Main extends scala.scalajs.js.JSApp {
   def refresh(client: Client): Unit = {
     client.getAttribute("java.lang:type=Memory", "HeapMemoryUsage", (data: Dynamic) => {
       val used = data.getSync("used")
-      println(s"HeapMemoryUsage used: $used")
+      // println(s"HeapMemoryUsage used: $used")
     })
 
     client.getAttribute("java.lang:type=Threading", "ThreadCount", (count: Dynamic) => {
-      println(s"ThreadCount: $count")
+      // println(s"ThreadCount: $count")
     })
 
     client.getAttribute("java.lang:type=ClassLoading", "TotalLoadedClassCount", (count: Dynamic) => {
-      println(s"TotalLoadedClassCount: $count")
+      // println(s"TotalLoadedClassCount: $count")
     })
 
     // HEAP
 
     client.getAttribute("java.lang:type=MemoryPool,name=PS Eden Space", "Usage", (data: Dynamic) => {
       val used = data.getSync("used")
-      println(s"PS Eden Space used: $used")
+      // println(s"PS Eden Space used: $used")
     })
 
     client.getAttribute("java.lang:type=MemoryPool,name=PS Survivor Space", "Usage", (data: Dynamic) => {
       val used = data.getSync("used")
-      println(s"PS Survivor Space used: $used")
+      // println(s"PS Survivor Space used: $used")
     })
 
     client.getAttribute("java.lang:type=MemoryPool,name=PS Old Gen", "Usage", (data: Dynamic) => {
       val used = data.getSync("used")
-      println(s"PS Old Gen used: $used")
+      // println(s"PS Old Gen used: $used")
     })
 
     // NON_HEAP
 
     client.getAttribute("java.lang:type=MemoryPool,name=Code Cache", "Usage", (data: Dynamic) => {
       val used = data.getSync("used")
-      println(s"Code Cache used: $used")
+      // println(s"Code Cache used: $used")
     })
 
     client.getAttribute("java.lang:type=MemoryPool,name=Compressed Class Space", "Usage", (data: Dynamic) => {
       val used = data.getSync("used")
-      println(s"Compressed Class Space used: $used")
+      // println(s"Compressed Class Space used: $used")
     })
 
     client.getAttribute("java.lang:type=MemoryPool,name=Metaspace", "Usage", (data: Dynamic) => {
       val used = data.getSync("used")
-      println(s"Metaspace used: $used")
+      // println(s"Metaspace used: $used")
     })
   }
 
@@ -86,7 +86,7 @@ object Main extends scala.scalajs.js.JSApp {
     val blessed = global.require("blessed")
     val contrib = global.require("blessed-contrib")
 
-    println(s"Creating widgets")
+    // println(s"Creating widgets")
 
     val screen = blessed.screen()
 
